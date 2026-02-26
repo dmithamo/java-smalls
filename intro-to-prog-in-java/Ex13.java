028c5b7dc9c1512a7b9d1401a7676ecdc6149631
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-02-26 10:40:58 +0300

    prog 1.3.9 refactor to use mordern language constructs for simple progs

--- intro-to-prog-in-java/Ex13.java
+++ intro-to-prog-in-java/Ex13.java
@@ -1,7 +1,8 @@
 public class Ex13 {
-  public static void main(String[] args) {
+  void main(String[] args) {
     // ToBinary.main(args);
-    PrimeFactors.main(args);
+    PrimeFactors p = new PrimeFactors();
+    p.main(args);
   }
 }
 
@@ -10,19 +11,19 @@ public class Ex13 {
  * of the convert to binary example (Prog 1.3.7)
  */
 class ToBinary {
-  public static void main(String[] args) {
+  void main(String[] args) {
     if (args.length == 0) {
       return;
     }
     if (args.length >= 1) {
       for (String a : args) {
-        System.out.println(ToBinary.makeBinary(a));
+        IO.println(this.makeBinary(a));
       }
       return;
     }
   }
 
-  public static String makeBinary(String asDec) {
+  String makeBinary(String asDec) {
     int decimal = Integer.parseInt(asDec);
     int SYSTEM_SIZE = 8;
     int pos = SYSTEM_SIZE - 1;
@@ -41,7 +42,7 @@ class ToBinary {
         theBinary += "0";
       } else {
         theBinary += "1";
-        valueOfBin += (int)posValue;
+        valueOfBin += (int) posValue;
       }
       pos -= 1;
     }
@@ -51,10 +52,10 @@ class ToBinary {
 
 /*
  * PrimeFactors (My impln of PROGRAM 1.3.9) computes the prime factorization of
-any given positive integer
- * */
+ * any given positive integer
+ */
 class PrimeFactors {
-  public static void main(String[] args) {
+  void main(String[] args) {
     long n = Long.parseLong(args[0]);
     String factors = "";
 
@@ -97,6 +98,6 @@ class PrimeFactors {
         factors += n;
       }
     }
-    System.out.println(factors);
+    IO.println(factors);
   }
 }
