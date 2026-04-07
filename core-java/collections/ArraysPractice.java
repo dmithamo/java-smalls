65693fe984fa8163c54278a8133d4411dc05789a
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-04-07 13:37:49 +0300

    [core-java] introduce meselft to java arrays

---
+++ core-java/collections/ArraysPractice.java
@@ -0,0 +1,8 @@
+public class ArraysPractice {
+  public static void main(String[] args) {
+    for (String arg : args) {
+      arg = arg.equals("-h") ? "Hello" : arg.equals("-g") ? "Goodbye" : arg;
+      System.out.print(arg + " ");
+    }
+  }
+}
