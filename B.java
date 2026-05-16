7716c59682e81d883d7833e6536ff8cb311a2069
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-05-16 17:53:13 +0300

    feat: an exercise on strings and input

--- B.java
+++ B.java
@@ -1,10 +1,12 @@
 class B {
-  static int age = 30;
+  static int age;
+  public final static double HEIGHT = 7.5;
   public static String name = "Dennis";
   private static String password = "Mithamo";
   public String rpassword = "Mithamo";
 
   public static void main(String[] args) {
     System.out.println("Password from inside B: " + password);
+    System.out.println("HEIGHT: " + HEIGHT);
   }
 }
---
+++ Exercises-4-dje.java
@@ -0,0 +1,23 @@
+class Exercises4DJE {
+  public static void main(String[] args) {
+    String input = IO.readln();
+    String capitalised = capitalise(input);
+    System.out.println("%s -> %s".formatted(input, capitalised));
+  }
+
+  public static String capitalise(String src) {
+    String[] words = src.split("\\s+");
+
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].isEmpty())
+        continue;
+
+      char[] wc = words[i].toCharArray();
+
+      wc[0] = Character.toUpperCase(wc[0]);
+      words[i] = String.valueOf(wc);
+    }
+
+    return String.join(" ", words);
+  }
+}
