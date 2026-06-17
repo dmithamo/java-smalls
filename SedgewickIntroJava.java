21d0eb40ffc3069c164c04d84bafa369983b238f
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-06-17 15:25:18 +0300

    feat: use dynamic programming to help recursion

--- SedgewickIntroJava.java
+++ SedgewickIntroJava.java
@@ -1,6 +1,10 @@
 public class SedgewickIntroJava {
   public static void main() {
-    System.out.println("Recurssion.gcd(102, 68) = " + Recurssion.gcd(102, 68));
+    // System.out.println("Recurssion.gcd(102, 68) = " + Recurssion.gcd(102, 68));
+
+    for (int i = 0; i <= 92; i++) {
+      System.out.println("DynamicProgrammingFib.fib(%d) =  %d".formatted(i, DynamicProgrammingFib.fib(i)));
+    }
   }
 
   public static class Recurssion {
@@ -14,6 +18,28 @@ public class SedgewickIntroJava {
         return gcd(j, i % j);
       }
       return gcd(j, i);
+    }
+  }
+
+  public static class DynamicProgrammingFib {
+    private static final int MAX = 92;
+    private static long[] cache = new long[MAX];
+
+    public static long fib(int n) {
+      if (n > MAX)
+        throw new Error("Too big");
+
+      if (n <= 0)
+        return 0;
+      if (n == 1)
+        return 1;
+
+      if (cache[n - 1] > 0) {
+        return cache[n - 1];
+      }
+
+      cache[n - 1] = fib(n - 1) + fib(n - 2);
+      return cache[n - 1];
     }
   }
 }
